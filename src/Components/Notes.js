4c38e51a07ce415f5eaa634a27d3ba99a055334a
Esha Patel <Patellesha11@gmail.com>
2025-04-29 08:47:50 +0300

    added const

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -2,5 +2,7 @@ import { useState, useEffect } from "react";
 import "./Notes.css";
 
 function Notes() {
-    
+    const [notes, setNotes] = useState(() => {
+        const saved = localStorage.getItem("my-notes")
+    })
 }
