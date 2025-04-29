ab96a13683c28828d04496289c35ee89c5fe74ae
Esha Patel <Patellesha11@gmail.com>
2025-04-29 08:48:36 +0300

    added JSON parse

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -4,5 +4,7 @@ import "./Notes.css";
 function Notes() {
     const [notes, setNotes] = useState(() => {
         const saved = localStorage.getItem("my-notes")
+        return saved ? JSON.parse(saved) : [];
+        
     })
 }
