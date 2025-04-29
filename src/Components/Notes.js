80c943e48354df2ebd79dd540d7f27d563a6d338
Esha Patel <Patellesha11@gmail.com>
2025-04-29 08:56:20 +0300

    added UseEffect

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -1,10 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import "./Notes.css";
 
 function Notes() {
     const [notes, setNotes] = useState(() => {
         const saved = localStorage.getItem("my-notes")
         return saved ? JSON.parse(saved) : [];
-        
-    })
+    });
+    const [input , setInput] = useState("");
+
+    useEffect(() => {
+        localStorage.setItem('my-notes', JSON.stringify(notes));
+      }, [notes]);
+
+    
+
 }
