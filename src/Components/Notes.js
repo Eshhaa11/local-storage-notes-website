c1709ca6252cf0259a9470ad0d195b164b67cc79
Esha Patel <Patellesha11@gmail.com>
2025-04-29 09:07:39 +0300

    Done

--- src/Components/Notes.js
+++ src/Components/Notes.js
@@ -2,51 +2,49 @@ import { useState, useEffect, use } from "react";
 import "./Notes.css";
 
 function Notes() {
-    const [notes, setNotes] = useState(() => {
-        const saved = localStorage.getItem("my-notes")
-        return saved ? JSON.parse(saved) : [];
-    });
-    const [input , setInput] = useState("");
+  const [notes, setNotes] = useState(() => {
+    const saved = localStorage.getItem("my-notes");
+    return saved ? JSON.parse(saved) : [];
+  });
+  const [input, setInput] = useState("");
 
-    useEffect(() => {
-        localStorage.setItem('my-notes', JSON.stringify(notes));
-      }, [notes]);
+  useEffect(() => {
+    localStorage.setItem("my-notes", JSON.stringify(notes));
+  }, [notes]);
 
-    const addNote = () => {
-        if (input.trim()) {
-            setNotes([input, ...notes]);
-            setInput("");
+  const addNote = () => {
+    if (input.trim()) {
+      setNotes([input, ...notes]);
+      setInput("");
+    }
+  };
 
-        }
-    };
+  const deleteNote = (index) => {
+    const newNotes = notes.filter((_, i) => i !== index);
+    setNotes(newNotes);
+  };
 
-    const deleteNote = (index) => {
-        const newNotes = notes.filter((_, i) => i !== index);
-        setNotes(newNotes);
-    };
+  return (
+    <div className="notes-cont">
+      <div className="notes-input">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Write your notes here..."
+        />
 
-    return (
-        <div className="notes-cont">
-            <div className="notes-input">
-                < input 
-                value={input}
-                onChange={(e) => setInput(e.target.value)}
-                placeholder="Write your notes here..."
-                />
-
-                <button onClick={addNote}>Add Note</button>
-            </div> 
-            <div className="note-list">
-            {notes.map((note, index) => (
-                 <div className="note" key={index}>
-                      <p>{note}</p>
-                      <button onClick={() => deleteNote(index)}>🗑️</button>      
-                      </div>
+        <button onClick={addNote}>Add Note</button>
+      </div>
+      <div className="note-list">
+        {notes.map((note, index) => (
+          <div className="note" key={index}>
+            <p>{note}</p>
+            <button onClick={() => deleteNote(index)}>🗑️</button>
+          </div>
         ))}
-      </div>   
+      </div>
     </div>
-    );
-
-};
+  );
+}
 
 export default Notes;
